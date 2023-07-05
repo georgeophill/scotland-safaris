a61bbe87fecff0de15f40c2f813a5bbf058c0a23
George Hill <georgeophill@outlook.com>
2023-07-05 15:35:27 +0100

    css for main index

--- scotland-safaris/scotland-safaris/src/App.jsx
+++ scotland-safaris/scotland-safaris/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Guides from './pages/guides/Guides'
+import Safari from './pages/safaris/Safaris'
+import NotFound from './pages/notFound/NotFound'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>You are gay</div>
+      <BrowserRouter>
+        <Navbar />
+        <Home />
+        <About />
+        <Contact />
+        <Gallery />
+        <Guides />
+        <Safari />
+        <NotFound />
+      </BrowserRouter>
   )
 }
 
---
+++ scotland-safaris/scotland-safaris/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import {links} from '../data'
+import {FaBars} from 'react-icons/fa'
+import './Navbar.css'
+import Logo from '../images/logo-stag.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className="logo">
+                <img src={Logo} alt="Logo" />
+            </Link>
+            <ul className='nav__links'>
+              {
+                links.map(({ name, path}, index) => {
+                  return (
+                    <li>
+                      <NavLink to={path}> {name} </NavLink>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+            <button className="nav__toggle-btn">
+                <FaBars />
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
