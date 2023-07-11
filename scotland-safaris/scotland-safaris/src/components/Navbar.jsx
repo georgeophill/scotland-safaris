032f10ee98c9098e6600dc8ef5b2ea00c431d372
George Hill <georgeophill@outlook.com>
2023-07-11 15:48:14 +0100

    Navbar built and responsive

--- scotland-safaris/scotland-safaris/src/components/Navbar.jsx
+++ scotland-safaris/scotland-safaris/src/components/Navbar.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import {useState, react }from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import {links} from '../data'
 import {FaBars} from 'react-icons/fa'
+import { GrClose } from 'react-icons/gr'
 import './Navbar.css'
 import Logo from '../images/logo-round.png'
 
 const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false)
   return (
     <nav>
         <div className="container nav__container">
             <Link to="/" className="logo">
                 <img src={Logo} alt="Logo" />
             </Link>
-            <ul className='nav__links'>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}> 
               {
                 links.map(({ name, path}, index) => {
                   return (
-                    <li>
+                    <li key={index}>
                       <NavLink to={path} className = {({isActive}) => isActive ? 'active-nav' : '' }> {name} </NavLink>
                     </li>
                   )
                 })
               }
             </ul>
-            <button className="nav__toggle-btn">
-                <FaBars />
+            <button className="nav__toggle-btn" onClick={() => setIsNavShowing (!isNavShowing)}>
+              {
+                isNavShowing ? <GrClose /> : <FaBars />
+              }
             </button>
         </div>
     </nav>
