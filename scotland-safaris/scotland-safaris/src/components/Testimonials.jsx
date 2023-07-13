fa76b67ab18e5482689aeee2608ff64262de2dd9
George Hill <georgeophill@outlook.com>
2023-07-13 19:00:15 +0100

    setting up testimonials

--- scotland-safaris/scotland-safaris/src/components/Testimonials.jsx
+++ scotland-safaris/scotland-safaris/src/components/Testimonials.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import SectionHead from "./SectionHead"
+import { ImQuotesLeft } from 'react-icons/im'
+import Card from "../UI/Card"
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from 'react-icons/io'
+import { testimonials } from '../data'
+import { useState } from "react"
 
 const Testimonials = () => {
+    const [ index, setIndex ] = useState(0)
+    const {name, quotes, job, avatar} =  testimonials[index]
   return (
-    <div>Testimonials</div>
+    <section className="testimonials">
+        <div className="container testimonials__container"></div>
+        <SectionHead icon={ ImQuotesLeft } title="Testimonials" />
+        <Card className="testimonial">
+            <div className="testimonial__avatar">
+                <img src={ avatar } alt={ name } />
+            </div>
+        </Card>
+    </section>
   )
 }
 
