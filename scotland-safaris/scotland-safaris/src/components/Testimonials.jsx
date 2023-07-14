c0983893818e12e00be340b7c524557f310fac23
George Hill <georgeophill@outlook.com>
2023-07-14 12:15:17 +0100

    testimonial card styling

--- scotland-safaris/scotland-safaris/src/components/Testimonials.jsx
+++ scotland-safaris/scotland-safaris/src/components/Testimonials.jsx
@@ -6,17 +6,24 @@ import { testimonials } from '../data'
 import { useState } from "react"
 
 const Testimonials = () => {
-    const [ index, setIndex ] = useState(0)
-    const {name, quotes, job, avatar} =  testimonials[index]
+    const [ index, setIndex ] = useState(1)
+    const {name, quote, job, avatar} =  testimonials[index]
   return (
     <section className="testimonials">
         <div className="container testimonials__container"></div>
-        <SectionHead icon={ ImQuotesLeft } title="Testimonials" />
+        <SectionHead icon={ ImQuotesLeft } title="Testimonials" className="testimonials__head"/>
         <Card className="testimonial">
             <div className="testimonial__avatar">
                 <img src={ avatar } alt={ name } />
             </div>
+            <p className="testimonial__quote">{`"${quote}"`}</p>
+            <h5>{name}</h5>
+            <small className="testimonial__title">{job}</small>
         </Card>
+        <div className="testimonials__btn-container">
+            <button className="testimonials__btn"><IoIosArrowDropleftCircle/></button>
+            <button className="testimonials__btn"><IoIosArrowDroprightCircle/></button>
+        </div>
     </section>
   )
 }
