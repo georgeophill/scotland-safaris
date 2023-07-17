2d6142276a8c1f2ba2e537a7f45b3318419d6d7d
George Hill <georgeophill@outlook.com>
2023-07-17 11:45:44 +0200

    Added footer component, no styling

--- scotland-safaris/scotland-safaris/src/components/Footer.jsx
+++ scotland-safaris/scotland-safaris/src/components/Footer.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo1.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container footer__container">
+        <article>
+          <Link to="/" className='logo' >
+            <img src={Logo} alt="Footer logo" />
+          </Link>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Numquam, doloremque voluptas vitae cupiditate fuga quasi quaerat quis 
+            maiores neque labore?
+          </p>
+          <div className="footer__socials">
+            <a href="https://www.linkedin.com/in/orlando-hill-5290431b3/" target="_blank" rel='noreferrer'><FaLinkedin /></a>
+            <a href="https://www.instagram.com/scotlandsafaris/" target="_blank" rel='noreferrer'><AiFillInstagram /></a>
+            <a href="https://www.facebook.com/orlando.hill.18" target="_blank" rel='noreferrer'><FaFacebook /></a>
+            <a href="" target="_blank" rel='noreferrer'><AiOutlineTwitter /></a>
+          </div>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/safaris">Safaris</Link>
+          <Link to="/guides">Guides</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Insights</h4>
+          <Link to="/s">Blog</Link>
+          <Link to="/s">Case Studies</Link>
+          <Link to="/s">Events</Link>
+          <Link to="/s">Communities</Link>
+          <Link to="/s">FAQs</Link>
+        </article>
+        <article>
+          <h4>Get In Touch</h4>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/s">Support</Link>
+        </article>
+      </div>
+      <div className="footer__copyright">
+        <small>{currentYear} Scotland Safaris &copy; All Rights Reserved </small>
+      </div>
+    </footer>
   )
 }
 
