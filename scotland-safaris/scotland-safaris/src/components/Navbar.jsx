2241ea397289a2f775c55ff0ebcd6f9c5fe61165
George Hill <georgeophill@outlook.com>
2023-07-19 15:49:13 +0100

    added navbar transition of opacity

--- scotland-safaris/scotland-safaris/src/components/Navbar.jsx
+++ scotland-safaris/scotland-safaris/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if(window.scrollY > 250){
+      if(window.scrollY > 150){
         setIsScrolled(true);
       } else {
         setIsScrolled(false)
