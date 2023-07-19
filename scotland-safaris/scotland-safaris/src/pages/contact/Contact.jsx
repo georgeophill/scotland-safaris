006ca77653221ebfa0ee58f1f3a05f9ce23066a0
George Hill <georgeophill@outlook.com>
2023-07-19 19:58:18 +0100

    added gallery

--- scotland-safaris/scotland-safaris/src/pages/contact/Contact.jsx
+++ scotland-safaris/scotland-safaris/src/pages/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/bluebells.jpeg'
 import { MdEmail } from 'react-icons/md'
-import { BsMessenger } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
 import { IoLogoWhatsapp } from 'react-icons/io' 
 
 import './Contact.css'
@@ -9,9 +9,20 @@ import './Contact.css'
 const Contact = () => {
   return (
     <>
-    <Header title="Get In Touch" image={HeaderImage}>
-
+    <Header title="Get In Touch" image={HeaderImage} id="contactimage" >
+    Connect with us today! Please do not hesitate to contact us through our seamless communication experience. Reach out for inquiries, collaborations, or support. We're just a click away!
     </Header>
+    <section className='contact'>
+      <div className='container contact__container'>
+        <div className='contact__wrapper'>
+          <a href="mailto:orlandohill1997@gmail.com" target='_blank' rel='noreferrer noopener'> <MdEmail/> </a>
+          <a href="https://ig.me/m/scotlandsafaris/" target='_blank' rel='noreferrer noopener'> <BsInstagram/> </a>
+          <a href="https://wa.me/+447583161434" target='_blank' rel='noreferrer noopener'> <IoLogoWhatsapp/> </a>
+        </div>
+
+      </div>
+
+    </section>
     </>
   )
 }
--- scotland-safaris/scotland-safaris/src/pages/gallery/Gallery.jsx
+++ scotland-safaris/scotland-safaris/src/pages/gallery/Gallery.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/stag-and-strath.JPG'
 import './Gallery.css'
 
 const Gallery = () => {
+
+  const galleryLength = 11;
+  const images = []
+
+  for( let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery/gallery${i}.JPG`))
+  }
+
+  console.log(images)
+
   return (
-    <div>Gallery</div>
+    
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Beatae non repudiandae ducimus repellendus cupiditate unde. Temporibus eius fugit quis nihil.
+      </Header>
+      <section className='gallery'>
+        <div className='container gallery__container'>
+          {
+            images.map((image, index) => {
+              return <article key={index}>
+                <img src={image} alt={`Gallery Image ${index+1}`} />
+              </article>
+            })
+          }
+          
+        </div>
+      </section>
+    </>
   )
 }
 
